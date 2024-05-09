7d729d052538e96bee20cdbd149a643ab7c02e79
juramos <juramos@student.42madrid.com>
2024-05-09 11:12:56 +0200

    added init

--- philo/init.c
+++ philo/init.c
@@ -1,23 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/08 12:54:21 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/09 10:42:23 by juramos          ###   ########.fr       */
+/*   Created: 2024/05/09 10:45:01 by juramos           #+#    #+#             */
+/*   Updated: 2024/05/09 10:45:02 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
-
-int	main(int argc, char **argv)
-{
-	t_rules	*rules;
-
-	if (argc != 5 && argc != 6)
-		return (print_error("Wrong number of arguments\n"));
-	init_all(rules);
-	(void) argv;
-}
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:55:36 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/08 13:43:50 by juramos          ###   ########.fr       */
+/*   Updated: 2024/05/09 10:44:45 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,14 @@
 
 /*	error	*/
 int	print_error(char *str);
+
+typedef struct s_rules
+{
+	int	nb_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	meals_needed;
+}				t_rules;
 
 #endif
