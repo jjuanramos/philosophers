5ee45a209c053942acad5f47a50d3fa5de4410dc
jjuanramos <jjuanramos91@gmail.com>
2024-05-20 10:25:10 +0200

    added structure

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:55:36 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/17 17:01:36 by juramos          ###   ########.fr       */
+/*   Updated: 2024/05/20 10:23:56 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,46 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+struct	s_rules;
+
 typedef struct s_philo
 {
-	int			nb;
-	long long	last_meal;
-	int			n_meals;
-}				t_philo;
+	int				id;
+	int				lf_id;
+	int				rf_id;
+	long long		last_meal;
+	int				n_meals;
+	struct s_rules	*rules;
+	pthread_t		thread_id;
+}					t_philo;
 
 typedef struct s_rules
 {
-	int		nb_philo;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		meals_needed;
-	t_philo	**philos;
-}				t_rules;
+	int				nb_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				meals_needed;
+	int				dead;
+	int				all_ate;
+	long long		starting_time;
+	t_philo			**philos;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	logger;
+	pthread_mutex_t	meal_check;
+}					t_rules;
 
 /*	error			*/
-void		rules_cleaner(t_rules *rules);
-int			print_error(t_rules *rules, char *str, int to_free);
+void				rules_cleaner(t_rules *rules);
+int					print_error(t_rules *rules, char *str, int to_free);
 /*	init			*/
-int			init_all(t_rules *rules, char **argv);
+int					init_all(t_rules *rules, char **argv);
 /*	time			*/
-long long	current_timestamp(void);
+long long			current_timestamp(void);
 /*	utils			*/
-int			ft_atoi(const char *str);
+int					ft_atoi(const char *str);
 
 /*	launch_threads	*/
-int			launch_threads(t_rules *rules);
+int					launch_threads(t_rules *rules);
 
 #endif
--- philo/src/init.c
+++ philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:45:01 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/17 17:03:27 by juramos          ###   ########.fr       */
+/*   Updated: 2024/05/20 10:19:45 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,25 @@ int	init_philos(t_rules *rules)
 	while (i < rules->nb_philo)
 	{
 		rules->philos[i] = malloc(2 * sizeof(t_philo));
-		rules->philos[i]->nb = i + 1;
+		rules->philos[i]->id = i + 1;
 		rules->philos[i]->n_meals = 0;
-		rules->philos[i]->last_meal = current_timestamp();
+		rules->philos[i]->last_meal = 0;
+		rules->philos[i]->rules = rules;
+		if (!i)
+		{
+			rules->philos[i]->rf_id = rules->nb_philo - 1;
+			rules->philos[i]->lf_id = 1;
+		}
+		else if (i == rules->nb_philo - 1)
+		{
+			rules->philos[i]->rf_id = rules->nb_philo - 2;
+			rules->philos[i]->lf_id = 0;
+		}
+		else
+		{
+			rules->philos[i]->rf_id = i - 1;
+			rules->philos[i]->lf_id = i + 1;
+		}
 		i++;
 	}
 	return (EXIT_SUCCESS);
@@ -37,13 +53,17 @@ int	init_all(t_rules *rules, char **argv)
 	rules->time_to_die = ft_atoi(argv[2]);
 	rules->time_to_eat = ft_atoi(argv[3]);
 	rules->time_to_sleep = ft_atoi(argv[4]);
-	if (argv[5])
-		rules->meals_needed = ft_atoi(argv[5]);
-	else
-		rules->meals_needed = -1;
 	if (rules->nb_philo < 1 || rules->time_to_die < 0
 		|| rules->time_to_eat < 0 || rules->time_to_sleep < 0)
 		return (print_error(rules, "Init. error: philo's args\n", 0));
+	if (argv[5])
+	{
+		rules->meals_needed = ft_atoi(argv[5]);
+		if (rules->meals_needed < 0)
+			return (print_error(rules, "Init. error: philo's args\n", 0));
+	}
+	else
+		rules->meals_needed = -1;
 	if (init_philos(rules))
 		return (print_error(rules, "Init. error: rules->philo's alloc\n", 1));
 	return (EXIT_SUCCESS);
