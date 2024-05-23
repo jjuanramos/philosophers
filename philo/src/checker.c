89591c45872baf5d8942d9d5dd65dc2405d72a1f
jjuanramos <jjuanramos91@gmail.com>
2024-05-23 18:49:59 +0200

    added mutexes again

--- philo/src/checker.c
+++ philo/src/checker.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:18:05 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/23 18:46:38 by juramos          ###   ########.fr       */
+/*   Updated: 2024/05/23 18:49:18 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,11 @@ static void	check_if_all_ate(t_rules *r, t_philo *p)
 		i++;
 	pthread_mutex_unlock(&(r->meal_check));
 	if (i == r->nb_philo)
+	{
+		pthread_mutex_lock(&(r->meal_check));
 		r->all_ate = 1;
-	usleep(50);
+		pthread_mutex_unlock(&(r->meal_check));
+	}
 }
 
 int	check_condition(int *val, pthread_mutex_t *mut)
